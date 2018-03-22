fe86f7ae5e70f203e310629b0c9e060ae38d6729
madooding <s7070112@kmitl.ac.th>
2018-03-23 05:10:24 +0700

    Update chord suggestions feature

--- src/components/Studio/ChordSuggestions.vue
+++ src/components/Studio/ChordSuggestions.vue
@@ -2,6 +2,30 @@
     <transition name="track-options" mode="out-in">
         <div class="track-options chord-suggestions" v-show="show">
             <div class="close-btn"><i class="ion-close" @click="toggle()"></i></div>
+            <div class="options-container">
+                <div class="chord-section">
+                    <div class="topic">Suggested Chords</div>
+                    <div class="group-btn">
+                        <div class="chord">Silence</div>
+                        <div class="chord">Dm</div>
+                        <div class="chord">Em</div>
+                        <div class="chord">F</div>
+                        <div class="chord">G</div>
+                        <div class="chord">Am</div>
+                        <div class="chord">Bdim</div>
+                        <div class="chord">Bb</div>
+                    </div>
+                </div>
+                <div class="duration-section">
+                    <div class="topic">Duration</div>
+                    <div class="group-btn">
+                        <div class="duration active">4/4</div>
+                        <div class="duration">3/4</div>
+                        <div class="duration">2/4</div>
+                        <div class="duration">1/4</div>
+                    </div>
+                </div>
+            </div>
         </div>
     </transition>
 </template>
